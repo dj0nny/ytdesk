6ab1b12e901317bb814035f1fa0d1ca5360ce51c
Dj0nny <frank96990@gmail.com>
2018-12-27 13:07:15 +0100

    add channel button, add channels array

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,40 +4,42 @@ import { DataService } from '../../data.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: [ './home.component.scss' ]
 })
 export class HomeComponent implements OnInit {
+	channelInfo: any;
+	channelSubscription: Subscription;
+	ChannelList: Array<String> = []; // channels Array
 
-  channelInfo: any;
-  channelSubscription: Subscription;
+	constructor(private electron: ElectronService, private data: DataService) {}
 
-  constructor(private electron: ElectronService, private data:DataService) { }
+	ngOnInit() {
+		this.channel('');
+	}
 
-  ngOnInit() {
-    this.channel('UCVyRiMvfUNMA1UPlDPzG5Ow')
-  }
+	closeWindow() {
+		this.electron.window.close();
+	}
 
-  closeWindow() {
-    this.electron.window.close();
-  }
+	minimizeWindow() {
+		this.electron.window.minimize();
+	}
 
-  minimizeWindow() {
-    this.electron.window.minimize();
-  }
+	channel(name) {
+	  // console.log('Start: ' + this.ChannelList);
 
-  channel(name) {
+		if (this.channelSubscription) this.channelSubscription.unsubscribe();
 
-    if(this.channelSubscription)
-      this.channelSubscription.unsubscribe();
+		this.ChannelList.push(name); // push the new channel ID into the array
+		// console.log('End: ' + this.ChannelList);
 
-    this.channelSubscription = this.data.getStats(name).subscribe((res) => {
-      this.channelInfo = res;
-      console.log(res);
-    })
-    
-  }
+		this.channelSubscription = this.data.getStats(this.ChannelList).subscribe((res) => { 
+			this.channelInfo = res;
+			console.log(res);
+		});
 
-
+		//console.log(name);
+	}
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,18 +4,22 @@ import { switchMap, map, startWith } from 'rxjs/operators';
 import { interval } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DataService {
+	constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getStats(name) {
-    return interval(5000)
-      .pipe(
-        startWith(0),
-        switchMap(() => this.http.get("https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id="+name+"&key=YOURAPIKEY")),
-        map(res => res)
-      )
-  }
+	getStats(name) {
+		return interval(5000).pipe(
+			startWith(0),
+			switchMap(() =>
+				this.http.get(
+					'https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=' +
+						name +
+						'&key=YOURAPIKEY'
+				)
+			),
+			map((res) => res)
+		);
+	}
 }
